d534a01d2e00ea48846964d4731491b8ffd6b2f7
matheusvoi-dev <matheus.voi@escola.pr.gov.br>
2026-05-13 19:21:28 +0000

    segundo

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
+const express = require("express");
+const path = require("path");
+const sqlite3 = require("sqlite3").verbose();
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-const db = new sqlite3.Database('./seu_nome.db'); // Use o nome exato do seu arquivo
-app.post('/salvar', (req, res) => {
+app.use(express.static(path.join(__dirname)));
+const db = new sqlite3.Database("./seu_banco.db"); // Use o nome exato do seu arquivo
+
+app.get("/", (req, res) => {
+res.sendFile(path.join(__dirname, "index.html"));
+});
+app.post("/salvar", (req, res) => {
 const { nome, telefone, email } = req.body;
 const sql = `INSERT INTO contatos (nome, telefone, email) VALUES (?, ?, ?)`;
-
-db.run(sql, [nome, telefone, email], function(err) {
+db.run(sql, [nome, telefone, email], function (err) {
 if (err) return res.send("Erro: " + err.message);
-res.send("<h1>Sucesso!</h1><p>Contato salvo no banco.</p><a href='/'>Voltar</a>");
+res.send(
+"<h1>Sucesso!</h1><p>Contato salvo no banco.</p><a href='/'>Voltar</a>",
+);
 });
 });
 app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
